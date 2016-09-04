fde734db9046baa1dfedd21410eb4dff080c207f
Jon Paredes <jonpared@gmail.com>
2016-09-04 10:28:49 -0700

    Iterate through array and hash using .each and .map! methods in iteration.rb file.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+#Release 0
 def make_name
 	puts "What's your first name?"
 	first_name = gets.chomp
@@ -8,3 +9,24 @@ end
 
 make_name { |first_name, last_name | puts "Great to meet you, #{first_name} #{last_name}!"}
 
+
+#Release 1
+cities = ['Chicago', 'London', 'Paris']
+
+profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
+
+cities.each do |city|
+	puts "#{city} is beautiful!"
+end
+
+profile.each do |key, value|
+	puts "known #{key} is #{value}"
+end
+
+p cities
+
+cities.map! do |city|
+	 city.reverse
+end
+
+p cities
