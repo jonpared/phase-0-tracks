4f4d1f8fd6a5859594ae9cc5c87547ab57ffb17f
Jon Paredes <jonpared@gmail.com>
2016-09-04 12:04:21 -0700

    Used documentation to apply different methods to modify arrays and hashes in iteration file

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,4 @@
-#Release 0
+#Release 0----------
 def make_name
 	puts "What's your first name?"
 	first_name = gets.chomp
@@ -10,7 +10,7 @@ end
 make_name { |first_name, last_name | puts "Great to meet you, #{first_name} #{last_name}!"}
 
 
-#Release 1
+#Release 1----------
 cities = ['Chicago', 'London', 'Paris']
 
 profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
@@ -30,3 +30,49 @@ cities.map! do |city|
 end
 
 p cities
+
+#Release 2----------
+
+#Number 1
+numbers = [1, 2, 3, 4, 5, 6 ,7]
+
+profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
+
+numbers.delete_if { |digit| digit < 4 }
+p numbers
+
+profile.delete_if { |key, value| value == 'house' }
+p profile
+
+#Number 2
+numbers = [1, 2, 3, 4, 5, 6 ,7]
+
+profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
+
+numbers.select! { |digit| digit > 5 }
+p numbers
+
+profile.select! { |key, value| value == 'L' }
+p profile
+
+#Number 3
+numbers = [1, 2, 3, 4, 5, 6 ,7]
+
+profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
+
+numbers.keep_if { |digit| digit > 5 }
+p numbers
+
+profile.keep_if { |key, value| value == 'L' }
+p profile
+
+#Number 4
+numbers = [1, 2, 3, 4, 5, 6 ,7]
+
+profile = { "Food" => "pizza", "Music" => "house", "Public Transportation" => "L"}
+
+numbers = numbers.take_while { |digit| digit < 3 }
+p numbers
+
+profile = profile.reject { |key, value| value == "pizza" }
+p profile
